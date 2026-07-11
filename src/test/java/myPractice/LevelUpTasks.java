0e5f3396f86da30537d39d0892468d17518449b3
Ahmed Mounir <amounir6000@gmail.com>
2026-07-11 19:04:02 +0300

    Refactor LevelUpTasks class to extend TesCases and move WebDriver setup to TesCases

--- src/test/java/myPractice/LevelUpTasks.java
+++ src/test/java/myPractice/LevelUpTasks.java
@@ -1,18 +1,11 @@
 package myPractice;
 
+import myPractice.templates.TesCases;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Collections;
-
-public class LevelUpTasks {
-    private WebDriver driver;
+public class LevelUpTasks extends TesCases {
 
     @Test
     public void navigateToGoogoleAndAssertPageTitleTask1(){
@@ -30,31 +23,4 @@ public class LevelUpTasks {
     }
 
 
-    @BeforeMethod
-    public void beforeMethod() {
-        driver = new ChromeDriver(getChromeOptions());
-    }
-    @AfterMethod
-    public void afterMethod() {
-        driver.quit();
-    }
-
-
-
-    private ChromeOptions getChromeOptions() {
-        ChromeOptions options = new ChromeOptions();
-        // Hides the "Chrome is being controlled by automated test software" banner
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-        options.addArguments("--disable-dev-shm-usage");
-        options.setAcceptInsecureCerts(true);
-// Disables browser extensions to speed up loading
-        options.addArguments("--disable-extensions");
-// mutes audio so your tests don't make noise during execution
-        options.addArguments("--mute-audio");
-// Starts Chrome maximized for consistent element positioning
-        options.addArguments("--start-maximized");
-// Reduces console noise from Chrome internal errors
-        options.addArguments("--log-level=3");
-        return options;
-    }
 }
---
+++ src/test/java/myPractice/templates/TesCases.java
@@ -0,0 +1,41 @@
+package myPractice.templates;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.Collections;
+
+public class TesCases {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver(getChromeOptions());
+    }
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        // Hides the "Chrome is being controlled by automated test software" banner
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.addArguments("--disable-dev-shm-usage");
+        options.setAcceptInsecureCerts(true);
+// Disables browser extensions to speed up loading
+        options.addArguments("--disable-extensions");
+// mutes audio so your tests don't make noise during execution
+        options.addArguments("--mute-audio");
+// Starts Chrome maximized for consistent element positioning
+        options.addArguments("--start-maximized");
+// Reduces console noise from Chrome internal errors
+        options.addArguments("--log-level=3");
+        return options;
+    }
+}
