620c3d738cbd8c0fdeaecb8427108feee2f10efd
Ahmed Mounir <amounir6000@gmail.com>
2026-07-11 21:16:04 +0300

    Add new test method to navigate to DuckDuckGo and assert first link result; rename existing test method for clarity

--- src/test/java/myPractice/LevelUpTasks.java
+++ src/test/java/myPractice/LevelUpTasks.java
@@ -2,6 +2,8 @@ package myPractice;
 
 import myPractice.templates.TesCases;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,11 +17,22 @@ public class LevelUpTasks extends TesCases {
         Assert.assertEquals(pageTitle,"Google");
     }
     @Test
-    public void navigateToDuckDuckGoAndAssertPageTitleTask2(){
+    public void navigateToDuckDuckGoAndAssertLogoIsDisplayedTask2(){
         driver.navigate().to("https://duckduckgo.com");
         Assert.assertTrue
                 (driver.findElement(By.xpath("(//a[@title='Learn about DuckDuckGo']//img)[2]"))
                         .isDisplayed(), "DuckDuckGo Logo is not displayed");
+    }
+    @Test
+    public void navigateToDuckDuckGoAndAssertTheFirstLinkResultTask3(){
+       driver.navigate().to("https://duckduckgo.com");
+       By searchText=By.id("searchbox_input");
+       driver.findElement(searchText).sendKeys("Selenium WebDriver", Keys.ENTER);
+       String expectedUrl = "https://www.selenium.dev/documentation/webdriver/";
+       Assert.assertTrue(
+               driver.findElement(By.xpath("(//a[@data-testid=\"result-extras-url-link\"])[1]"))
+                       .getAttribute("href")
+                       .equals(expectedUrl), "The first link result is not correct");
     }
 
 
